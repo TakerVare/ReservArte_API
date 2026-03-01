281de7af785b1b12cf92f9f59a89d08c9a66c59d
takervare <takervare@gmail.com>
2026-03-01 19:15:03 +0100

    Modificación del puerto del CORS para poder presentar con Óliver

--- ReservArte_API/Program.cs
+++ ReservArte_API/Program.cs
@@ -20,9 +20,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Configuración de CORS
 builder.Services.AddCors(options =>
 {
+    // options.AddPolicy("AllowFrontend", policy =>
+    // {
+    //     policy.WithOrigins("http://localhost:5173")
+    //           .AllowAnyHeader()
+    //           .AllowAnyMethod()
+    //           .AllowCredentials();
+    // });
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:8080")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
